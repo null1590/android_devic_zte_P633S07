64dae355a3eade49bd23df2e37725290cac89073
null1590 <52319097+null1590@users.noreply.github.com>
2022-12-23 15:46:16 +0800

    Add files via upload

---
+++ omni_P633S07.mk
@@ -0,0 +1,34 @@
+#
+# Copyright (C) 2020 The Android Open Source Project
+# Copyright (C) 2020 The TWRP Open Source Project
+# Copyright (C) 2020 SebaUbuntu's TWRP device tree generator
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# Inherit from P633S07 device
+$(call inherit-product, device/zte/P633S07/device.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := P633S07
+PRODUCT_NAME := omni_P633S07
+PRODUCT_BRAND := zte
+PRODUCT_MODEL := zte
+PRODUCT_MANUFACTURER := zte
+PRODUCT_RELEASE_NAME := zte zte
