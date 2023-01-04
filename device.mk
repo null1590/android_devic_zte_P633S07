d14ffa323f9c24f642acbccb2cc4df8ff98d0866
null1590 <52319097+null1590@users.noreply.github.com>
2023-01-04 21:20:23 +0800

    Update device.mk

--- device.mk
+++ device.mk
@@ -31,28 +31,32 @@ ENABLE_VIRTUAL_AB := true
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 
 # A/B
-AB_OTA_UPDATER := true
-AB_OTA_PARTITIONS += \
-boot \
-system \
-product \
-dtbo \
-vbmeta \
-vbmeta_system \
-vendor
+   AB_OTA_UPDATER := true
+   AB_OTA_PARTITIONS += \
+   boot \
+   system \
+   product \
+   dtbo \
+   vbmeta \
+   vbmeta_system \
+   vendor
 
 RODUCT_PACKAGES += \
-otapreopt_script \
-update_engine \
-update_engine_sideload \
-update_verifier
+   otapreopt_script \
+   update_engine \
+   update_engine_sideload \
+   update_verifier
 
 AB_OTA_POSTINSTALL_CONFIG += \
-RUN_POSTINSTALL_system = true \
-POSTINSTALL_PATH_system = system/bin/otapreopt_script \
-FILESYSTEM_TYPE_system = ext4 \
-POSTINSTALL_OPTIONAL_system = true
+    RUN_POSTINSTALL_system = true \
+    POSTINSTALL_PATH_system = system/bin/otapreopt_script \
+    FILESYSTEM_TYPE_system = ext4 \
+    POSTINSTALL_OPTIONAL_system = true
 
 # Boot control HAL
 PRODUCT_PACKAGES += \
-android.hardware.boot@1.1-impl.recovery
+    android.hardware.boot@1.0-impl \
+    android.hardware.boot@1.0-service
+
+PRODUCT_PACKAGES += \
+    bootctrl.mt6833
